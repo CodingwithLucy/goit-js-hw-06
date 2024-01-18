b9c13a5596bcbd3fee26fc73e8416685a9d00462
CodingwithLucy <143715585+CodingwithLucy@users.noreply.github.com>
2024-01-18 12:41:44 +0100

    add

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulItem = document.getElementById('ingredients');
+
+ingredients.forEach((ingredient) => {
+  const liItem = document.createElement("li");
+  liItem.textContent = ingredient;
+  liItem.classList.add("item");
+  ulItem.appendChild(liItem);
+})
